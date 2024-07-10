8662621586dc4cfcd9db09eef4885d1f9e2f8a64
Istiaque123 <istiaqueremon@gmail.com>
2024-07-10 23:32:03 +0600

    Update somethings

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,16 +1,19 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-mongoose.connect( "mongodb://localhost:27017/women_dress_onlineShop", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+dotenv.config();
 
+const MONGODB_URI = process.env.DB_URI || "mongodb://localhost:27017/women_dress_onlineShop";
 
-} ).then( () => {
-    console.log( `MongoDB connected successfully\n` );
-
-    
-} ).catch( ( err ) => {
-    console.error( `MongoDB connection error: ${err}` );
-} );
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('MongoDB connected successfully');
+  })
+  .catch((err) => {
+    console.error(`MongoDB connection error: ${err}`);
+  });
 
 export default mongoose;
